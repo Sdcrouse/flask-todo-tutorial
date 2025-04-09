ea2c8d88ee4b036d9b0d6da84e07cf0d7abab86c
Sdcrouse <sdcrouse91@gmail.com>
2025-04-08 21:19:41 -0700

    Configured a SQLite database and added a MyTask model

--- app.py
+++ app.py
@@ -2,13 +2,29 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 # My App
 app = Flask(__name__)
+Scss(app)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data class - Row of data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self) -> str:
+        # This provides a string representation of an object
+        return f"Task {self.id}"
 
 
-
+# Routes to webpages
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -16,6 +32,9 @@ def index():
 
 
 
-
-if __name__ in "__main__":
+# Runner and debugger
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    
     app.run(debug=True)
